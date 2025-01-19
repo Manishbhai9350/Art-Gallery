2b162d595d84cfcac7423e13f155874115a58de2
Manish <keshraram294@gmail.com>
2025-01-19 16:24:09 +0530

    Landing Page Done

---
+++ data.js
@@ -0,0 +1,34 @@
+export const Data = [
+    {
+        img:'./public/Images/img1.jpg',
+        title:'Nef-Rick'
+    },
+    {
+        img:'./public/Images/img2.jpg',
+        title:null
+    },
+    {
+        img:'./public/Images/img3.jpg',
+        title:'Devon'
+    },
+    {
+        img:'./public/Images/img4.jpg',
+        title:'Alex Franklin'
+    },
+    {
+        img:'./public/Images/img5.jpg',
+        title:null
+    },
+    {
+        img:'./public/Images/img5.jpg',
+        title:null
+    },
+    {
+        img:'./public/Images/img6.jpg',
+        title:'Trevor-Auto'
+    },
+    {
+        img:'./public/Images/img7.jpg',
+        title:'Cristiano The Man [7]'
+    },
+]
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Inpiration <a style="color:wheat" herf='https://dribbble.com/shots/22206428-Art-Galley-Website-Design'>Dribble.com</a>
+
